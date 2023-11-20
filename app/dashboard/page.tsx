c2603deb51709845dba809a1ed21ebe64e030745
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-11-20 23:21:11 +0300

    better enhancment

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,9 +7,9 @@ type Props = {}
 const Dashboard = (props: Props) => {
   return (
     <div className="flex justify-center py-24 min-h-screen  bg-gradient-to-r from-slate-950 via-gray-950 to-slate-950">
-        <div className="flex justify-center py-24 flex-row gap-x-7 max-w-xl">
+        <div className="flex justify-center flex-row gap-x-7 max-w-xl">
             <LayoutDashboard className="w-11 h-11 text-slate-50" />
-          <h1 className="text-5xl font-bold text-slate-50">Dashboard   
+          <h1 className="sm:text-5xl text-xl font-bold text-slate-50 mt-2 sm:mt-0">Dashboard   
           </h1>
           <div className="mt-2">
                 <UserButton  />
